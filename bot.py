63c946b7414c1a7c90ba8a64a7b75d183d67158d
brandon <brandonminhnguyen@yahoo.com>
2019-10-26 17:58:32 -0500

    stopping point

--- bot.py
+++ bot.py
@@ -1,15 +1,27 @@
+# bot.py
 import os
 
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-client.run(token)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+    # Stopping Point
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(guild)
+
+client.run(TOKEN)
