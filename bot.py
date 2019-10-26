983e445c8de0583b3de889f9e7aeb16778ac9520
poton8 <pagilestennis@gmail.com>
2019-10-26 17:19:54 -0500

    base code

--- bot.py
+++ bot.py
@@ -1,25 +1,15 @@
-# Work with Python 3.6
-import discord
+import os
 
-TOKEN = '######'
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
 @client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
-@client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print(f'{client.user} has connected to Discord!')
 
-client.run(TOKEN)
+client.run(token)
